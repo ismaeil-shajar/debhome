574687fc38ada8d1d77cce0e483102a97a6c8ab1
ismaeil-shajar <msherif.ali6@gmail.com>
2022-01-12 21:06:12 +0200

    Initial commit

---
+++ src/main/java/com/shajar/deb/DebApplication.java
@@ -0,0 +1,13 @@
+package com.shajar.deb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shajar/deb/DebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shajar.deb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
